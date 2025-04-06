d3b5ae56ca811adc160e7b5a0463f97f882ef7c4
albertalvin8080 <albertalvin99@gmail.com>
2025-04-06 15:27:50 -0300

    feat: unit testing

--- 04-unit-testing/src/app/hero-detail/hero-detail.component.spec.ts
+++ 04-unit-testing/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, fakeAsync, flush, TestBed, tick, waitForAsync } from "@angular/core/testing";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "../hero.service";
 import { Location } from "@angular/common";
@@ -39,4 +39,32 @@ describe('HeroDetailComponent', () => {
         expect(fixture.nativeElement.querySelector('h2').textContent)
             .toContain('detestatio sacrorum'.toUpperCase());
     });
+
+    it('should call updateHero when save is called', fakeAsync(() => {
+        mockHeroSvc.getHero.and.returnValue(of(
+            { id: 3, name: 'detestatio sacrorum', strength: 1e-5 }
+        ));
+        mockHeroSvc.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.save();
+        // tick(250); // forwards the inernal clock of the zone created by Zone.js
+        flush(); // if you don't know the time span, use this to execute pending tasks.
+
+        expect(mockHeroSvc.updateHero).toHaveBeenCalled();
+    }));
+
+    it('should call updateHero when savePromise is called', waitForAsync(() => {
+        mockHeroSvc.getHero.and.returnValue(of(
+            { id: 3, name: 'detestatio sacrorum', strength: 1e-5 }
+        ));
+        mockHeroSvc.updateHero.and.returnValue(of({}));
+        fixture.detectChanges();
+
+        fixture.componentInstance.savePromise();
+        // whenStable halts execution until all promises have been fulfilled.
+        fixture.whenStable().then(() => {
+            expect(mockHeroSvc.updateHero).toHaveBeenCalled();
+        });
+    }));
 });
